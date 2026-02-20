22ebcb6d417c125f3ef7f233c9fb3bef96bcdc47
galaxycc1 <galaxy.dgx.96@gmail.com>
2026-02-20 02:04:09 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,8 +1,2 @@
-const socialLinks = {
-  facebook: "https://www.facebook.com/share/1HgufB2S3v/",
-  instagram: "https://www.instagram.com/sideleipzig_p.c?igsh=MWxqa2t5Y3JzZWVmZQ==",
-  tiktok: "https://www.tiktok.com/@side_doner?lang=jv-ID&is_from_webapp=1&sender_device=mobile&sender_web_id=7590598014764844566",
-  website: "https://sidedoener-leipzig.de/",
-  online: "https://wolt.com/de/deu/leipzig/restaurant/side-doener-paunsdorf-center?utm_source=googlemapreserved&utm_campaign=side-doener-paunsdorf-center&utm_content=67ed4bc7faf402ff25420ca7&rwg_token=AFd1xnEETDb0uyNtDJosGkUBKJNknikSBYdAeB9ZEK_5QdWiJLdmJO-JNchEkq3OYdHLYlMvv9ARPVaHLiUFQqak_5O1smhRzrKfJRg4z-q4Q9YLtUHhdCI%3D",
-  map: "https://maps.app.goo.gl/DHLvFq7YiS4EtyeZ7"
-};
+// links.js
+// حاليًا فارغ، يمكن إضافة روابط ديناميكية مستقبلاً إذا أحببت
