c87cfa4807fc3ed034d80834c236ca372a189a9a
galaxycc1 <galaxy.dgx.96@gmail.com>
2026-02-22 19:36:41 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-// تبديل بين دارك مود و لايت مود
-const darkModeToggle = document.getElementById("darkModeToggle");
-darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
